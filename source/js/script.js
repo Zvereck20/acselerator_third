d1d77946e6e72168913a3a6ed5b7163ab9d92d4f
Zvereck20 <kerusov20@gmail.com>
2021-09-08 21:23:25 +1000

    Fresh

--- source/js/script.js
+++ source/js/script.js
@@ -1,216 +1,106 @@
-// Modal window
+// Menu
 
-const body = document.querySelector('body');
-const modal = document.querySelector('.modal');
-const closeModalButton = modal.querySelector('.modal__toggle');
-const openModalButton = document.querySelector('.navigation__button');
+const body = document.querySelector('main')
+const header = document.querySelector('.header');
+const navToggle = document.querySelector('.header__toggle');
 
-const MODAL_QUESTION = document.querySelector('#modal-question');
-const MODAL_NAME = modal.querySelector('#modal-name');
-const MODAL_PHONE = modal.querySelector('#modal-telephone');
-const MODAL_CHECKBOX = modal.querySelector('#modal-agree');
-const MODAL_BUTTON = modal.querySelector('.button');
+header.classList.add('header--js')
 
-function existVerticalScroll() {
-  return document.body.offsetHeight > window.innerHeight
-};
-
-function getBodyScrollTop() {
-  return self.pageYOffset || (document.documentElement && document.documentElement.ScrollTop) || (document.body && document.body.scrollTop);
-};
-
-const setTabindex = () => {
-  MODAL_NAME.setAttribute('tabindex', 1);
-  MODAL_PHONE.setAttribute('tabindex', 2);
-  MODAL_QUESTION.setAttribute('tabindex', 3);
-  MODAL_BUTTON.setAttribute('tabindex', 4);
-  MODAL_CHECKBOX.setAttribute('tabindex', 5);
-  closeModalButton.setAttribute('tabindex', 6);
+const closeMenu = () => {
+  (header.classList.contains('header--js')) ?
+  header.classList.remove('header--js'):
+    header.classList.add('header--js');
 }
 
-const removeTabindex = () => {
-  MODAL_NAME.removeAttribute('tabindex');
-  MODAL_PHONE.removeAttribute('tabindex');
-  MODAL_QUESTION.removeAttribute('tabindex');
-  MODAL_BUTTON.removeAttribute('tabindex');
-  MODAL_CHECKBOX.removeAttribute('tabindex');
-  closeModalButton.removeAttribute('tabindex');
-}
+navToggle.addEventListener('click', function () {
 
-openModalButton.addEventListener('click', e => {
-  e.preventDefault();
+  closeMenu();
 
-  body.dataset.scrollY = getBodyScrollTop();
-
-  modal.classList.remove('visually-hidden');
-
-  modal.querySelector('#modal-name').focus();
-
-  if (existVerticalScroll()) {
-    body.classList.add('body-lock')
-    body.style.top = `-${body.dataset.scrollY}px`
-  };
-
-  setTabindex();
-  focusLock.on(modal);
-})
-
-closeModalButton.addEventListener('click', e => {
-  e.preventDefault();
-
-  modal.classList.add('visually-hidden');
-
-  if (existVerticalScroll()) {
-    body.classList.remove('body-lock')
-    window.scrollTo(0, body.dataset.scrollY)
-  };
-
-  removeTabindex();
-  focusLock.off(modal);
-})
+  if (header.classList.contains('header--closed')) {
+    header.classList.remove('header--closed');
+    header.classList.add('header--opened');
+    body.classList.add('body-lock');
+  } else {
+    header.classList.add('header--closed');
+    header.classList.remove('header--opened');
+    body.classList.remove('body-lock');
+  }
+});
 
 document.addEventListener('keydown', (evt) => {
   if (evt.keyCode === 27) {
-    if (!modal.classList.contains('visually-hidden')) {
-      modal.classList.add('visually-hidden');
-    }
-
-    if (existVerticalScroll()) {
+    if (!header.classList.contains('header--closed')) {
+      header.classList.remove('header--opened');
+      header.classList.add('header--closed');
       body.classList.remove('body-lock')
-      window.scrollTo(0, body.dataset.scrollY)
     }
-
-    removeTabindex();
-    focusLock.off(modal);
-  };
-});
-
-modal.addEventListener('click', (evt) => {
-  if (evt.target === modal) {
-    modal.classList.add('visually-hidden');
+    closeMenu();
   }
+});
 
-  if (existVerticalScroll()) {
+const navLinks = document.querySelectorAll('.navigation__link');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    header.classList.add('header--closed');
+    header.classList.remove('header--opened');
     body.classList.remove('body-lock')
-    window.scrollTo(0, body.dataset.scrollY)
-  };
+    closeMenu();
+  })
+})
 
-  removeTabindex();
-  focusLock.off(modal);
-});
+// Swiper
 
-const checkedCheckbox = (ckeckbox) => {
-  if (!ckeckbox.checked) {
-    ckeckbox.setCustomValidity('К сожалению вы не дали своего согласия!');
-  } else {
-    ckeckbox.setCustomValidity('');
-  }
-}
 
-// Local save
+const swiper = new Swiper('.swiper-container', {
+  // slidesPerView: 4,
+  // slidesPerGroup: 4,
+  // slidesPerColumn: 4,
+  // slidesPerColumnFill:"row",
+  // spaceBetween: 30,
 
-MODAL_CHECKBOX.addEventListener('click', () => {
-  checkedCheckbox(MODAL_CHECKBOX);
-});
-
-modal.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-
-  localStorage.setItem('modal__name', MODAL_NAME.value);
-  localStorage.setItem('modal__phone', MODAL_PHONE.value);
-  localStorage.setItem('modal__question', MODAL_QUESTION.value);
-  MODAL_NAME.value = '';
-  MODAL_PHONE.value = '';
-  MODAL_QUESTION.value = 'Ваш вопрос';
-
-  modal.classList.add('visually-hidden');
-
-  if (existVerticalScroll()) {
-    body.classList.remove('body-lock')
-    window.scrollTo(0, body.dataset.scrollY)
-  };
-});
-
-const feedbackForm = document.querySelector('.feedback__field');
-const FEEDBACK_NAME = feedbackForm.querySelector('#name');
-const FEEDBACK_PHONE = feedbackForm.querySelector('#telephone');
-const FEEDBACK_QUESTION = feedbackForm.querySelector('#question');
-const FEEDBACK_CHECKBOX = feedbackForm.querySelector('#agree');
-
-FEEDBACK_CHECKBOX.addEventListener('click', () => {
-  checkedCheckbox(FEEDBACK_CHECKBOX);
-});
-
-feedbackForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-
-  localStorage.setItem('feedback__name', FEEDBACK_NAME.value);
-  localStorage.setItem('feedback__phone', FEEDBACK_PHONE.value);
-  localStorage.setItem('feedback__question', FEEDBACK_QUESTION.value);
-  FEEDBACK_NAME.value = '';
-  FEEDBACK_PHONE.value = '';
-  FEEDBACK_QUESTION.value = 'Ваш вопрос';
-});
-
-// Form validation
-
-window.addEventListener("DOMContentLoaded", function () {
-
-  function setCursorPosition(pos, elem) {
-    elem.focus();
-    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
-    else if (elem.createTextRange) {
-      var range = elem.createTextRange();
-      range.collapse(true);
-      range.moveEnd("character", pos);
-      range.moveStart("character", pos);
-      range.select()
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 30,
     }
-  };
+  },
 
-  function mask(event) {
-    var matrix = "+7 (___) ___ ____",
-      i = 0,
-      def = matrix.replace(/\D/g, ""),
-      val = this.value.replace(/\D/g, "");
+  navigation: {
+    nextEl: '.products__button--next',
+    prevEl: '.products__button--prewiev',
+  },
+  // ставми классы собственных кнопок, без доп от swiper
 
-    if (val.length < 10) {
-      this.setCustomValidity('Номер введен не полностью');
-    } else {
-      this.setCustomValidity('');
-    }
-
-    if (def.length >= val.length) val = def;
-    this.value = matrix.replace(/./g, function (a) {
-      return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
-    });
-
-    if (event.type == "blur") {
-      if (this.value.length == 2) this.value = ""
-    } else setCursorPosition(this.value.length, this)
-  };
-
-  const fields = document.querySelectorAll('input[type="tel"]');
-  fields.forEach((input) => {
-    input.addEventListener("input", mask, false);
-    input.addEventListener("focus", mask, false);
-    input.addEventListener("blur", mask, false);
-  });
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  //   type: 'fraction',
+  // },
 });
 
 
 // Accordion
 
 const accordion = document.querySelectorAll('.accordion');
-const accordionToggle = document.querySelectorAll('.toggle');
+const accordionToggle = document.querySelectorAll('.question__heading');
 
-accordion.forEach((item) => {
-  item.classList.add('accordion--close');
-});
+
+  accordion.forEach((item) => {
+    item.classList.add('accordion--close');
+  });
+
 
 const emergenceToggle = () => {
   accordionToggle.forEach((toggle) => {
-    toggle.classList.add('toggle--close');
+    toggle.classList.add('question__heading--close');
+    toggle.classList.remove('question__heading--open')
   })
 };
 
@@ -220,22 +110,27 @@ accordionToggle.forEach((toggle) => {
   toggle.addEventListener('click', () => {
 
     emergenceToggle();
+    // rem();
 
     const parent = toggle.parentNode;
 
+    console.log(parent);
+
     if (parent.classList.contains('accordion--close')) {
-      toggle.classList.remove('toggle--close');
-      toggle.classList.add('toggle--open');
+      toggle.classList.remove('question__heading--close');
+      toggle.classList.add('question__heading--open');
     } else {
-      toggle.classList.add('toggle--close');
-      toggle.classList.remove('toggle--open');
+      toggle.classList.remove('question__heading--open');
+      toggle.classList.add('question__heading--close');
     }
 
     if (parent.classList.contains('accordion--close')) {
-      accordion.forEach((item) => item.classList.add('accordion--close'));
+      accordion.forEach((item) =>
+      item.classList.add('accordion--close'));
       parent.classList.remove('accordion--close');
     } else {
       parent.classList.add('accordion--close');
+      console.log(item);
     }
   })
 });
